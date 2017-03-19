23769f5e9fc69bdee0eb9d093d7be1f4ece53085
gregpaton08 <gregpaton08@gmail.com>
2017-03-19 10:54:46 -0400

    Add function to get body text

--- parse_email.py
+++ parse_email.py
@@ -12,8 +12,9 @@ import datetime # used to parse bill due date
 def get_subject_for_message(message):
     return next((header for header in message['payload']['headers'] if header.get('name', '') == 'Subject'), {} ).get('value', 'email has not subject')
 
-# def get_body_for_message(message):
-#     body_data = message
+def get_body_for_message(message):
+    body_data = message['payload']['body'].get('data', '')
+    return base64.urlsafe_b64decode(body_data.encode('ascii'))
 
 
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
@@ -41,29 +42,28 @@ message_ids = GMAIL.users().messages().list(userId='me', q=query).execute().get(
 for message_id in message_ids:
     message = GMAIL.users().messages().get(userId='me', id=message_id['id']).execute()
 
-    print(get_subject_for_message(message))
+    subject = get_subject_for_message(message)
 
-    # print the body of the email
-    body_data = message['payload']['body'].get('data', None)
-    if body_data:
-        body_text = base64.urlsafe_b64decode(body_data.encode('ascii'))
-        
-        balance = ''
-        balance_search_token = 'current balance of $'
-        due_date = None
-        due_date_search_token = 'is due on '
-        body_lines = body_text.split('<br />')
-        for line in body_lines:
-            if balance_search_token in line:
-                balance = line[line.find(balance_search_token) + len(balance_search_token):]
-                balance = balance[:balance.find(' ')]
-                if due_date_search_token in line:
-                    date = line[line.find(due_date_search_token) + len(due_date_search_token):]
-                    date = date.translate(None, string.punctuation)
-                break
-        
-        # parse the date into a datetime object and decrement the month to get the correct billing cycle
-        billing_cycle_date = datetime.datetime.strptime(date, '%B %d %Y').date().replace(day=1) + datetime.timedelta(days=-1)
-        
-        print('Balance: ' + balance)
-        print(billing_cycle_date)
+    body_text = get_body_for_message(message)
+    
+    balance = ''
+    balance_search_token = 'current balance of $'
+    due_date = None
+    due_date_search_token = 'is due on '
+    body_lines = body_text.split('<br />')
+    for line in body_lines:
+        if balance_search_token in line:
+            balance = line[line.find(balance_search_token) + len(balance_search_token):]
+            balance = balance[:balance.find(' ')]
+            if due_date_search_token in line:
+                date = line[line.find(due_date_search_token) + len(due_date_search_token):]
+                date = date.translate(None, string.punctuation)
+            break
+    
+    # parse the date into a datetime object and decrement the month to get the correct billing cycle
+    billing_cycle_date = datetime.datetime.strptime(date, '%B %d %Y').date().replace(day=1) + datetime.timedelta(days=-1)
+    
+
+    print(subject)
+    print('Balance: ' + balance)
+    print(billing_cycle_date)
