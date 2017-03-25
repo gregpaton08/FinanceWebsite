71f92caa5656c2ce8fd05f928b62e691faa08b85
gregpaton08 <gregpaton08@gmail.com>
2017-03-25 15:43:58 -0400

    Add function to parse Amex emails

--- parse_email.py
+++ parse_email.py
@@ -40,8 +40,12 @@ def get_body_for_message(message):
         # Otherwise just take the first part
         if not part:
             part = parts[0]
+        else:
+            print('HTML version of body found')
 
         body_data = part.get('body', {}).get('data', '')
+    else:
+        print('Body found in body')
     return base64.urlsafe_b64decode(body_data.encode('utf-8'))
 
 def parse_pseg_message(message_body):
@@ -103,6 +107,43 @@ def parse_citi_message(message_body):
         'paid_date' : None
     }
 
+def parse_amex_message(message_body):
+    # print(message_body)
+    # Verify that this email contains statement information.
+    body_search_term = 'statement'
+    if not body_search_term in message_body.lower():
+        return None
+
+    tree = html.fromstring(message_body)
+
+    query = '//*/text()[contains(., "$")]'
+    results = tree.xpath(query)
+    balance = results[0].encode('ascii', 'ignore')
+    print(balance)
+    # balance = float(balance.replace('$', '').replace(',', ''))
+
+
+
+    query = '//td[child::text()[contains(., "Payment Due Date:")]]'
+    results = tree.xpath(query)
+    print(len(results))
+    print(results[0].text_content())
+    # billing_cycle_date = results[0].encode('ascii', 'ignore')
+    # print(billing_cycle_date)
+    # billing_cycle_date = datetime.datetime.strptime(billing_cycle_date, '%m/%d/%y').date()
+
+
+    due_date = None
+    billing_cycle_date = None
+
+    return {
+        'account' : 'Citi',
+        'balance' : balance,
+        'due_date' : due_date,
+        'billing_cycle' : billing_cycle_date,
+        'paid_date' : None
+    }
+
 def print_bill_info(bill_info):
     print('Account:       ' + bill_info['account'])
     print('Balance:       ' + str(bill_info['balance']))
@@ -112,7 +153,8 @@ def print_bill_info(bill_info):
 
 
 query_terms = {
-    'from:' : 'alerts@citibank.com'
+    'from:' : 'americanexpress@welcome.aexp.com'
+    # 'from:' : 'alerts@citibank.com'
     # 'from:' : 'myaccount@pseg.com'
 }
 
@@ -122,18 +164,28 @@ for key, val in query_terms.iteritems():
 
 # todo: update query to only check for emails with a date after the last time the script was called
 # Get a list of the message IDs using the list() method. Still need to get the message using the get() method and passing the ID.
-# gmail_service = get_gmail_service()
-message_ids = [0]#gmail_service.users().messages().list(userId='me', q=query).execute().get('messages', [])
+gmail_service = get_gmail_service()
+message_ids = gmail_service.users().messages().list(userId='me', q=query).execute().get('messages', [])
+print(str(len(message_ids)) + ' messages found')
 for message_id in message_ids:
-    # message = gmail_service.users().messages().get(userId='me', id=message_id['id']).execute()
+    message = gmail_service.users().messages().get(userId='me', id=message_id['id']).execute()
 
-    # subject = get_subject_for_message(message)
+    subject = get_subject_for_message(message)
+    if not 'statement' in subject.lower():
+        continue
+    print(subject)
 
-    body_text = ''#get_body_for_message(message)
-    with open('citi.html', 'r') as file:
-        body_text = file.read();
+    body_text = get_body_for_message(message)
+    # print(body_text)
+    # with open('amex.html', 'w') as file:
+    #     file.write(body_text)
 
-    bill_info = parse_citi_message(body_text)
-    print_bill_info(bill_info)
+    bill_info = parse_amex_message(body_text)
+    # print_bill_info(bill_info)
+
+    break
+
+    # bill_info = parse_citi_message(body_text)
+    # print_bill_info(bill_info)
     
     # bill_info = parse_pseg_message(body_text)
